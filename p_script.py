8a81b68af5ccb4565463d206fa2bd08a21acd2d1
sailorperrett <sailor.perrett@gmail.com>
2022-06-29 22:48:16 +1000

    flask

--- p_script.py
+++ p_script.py
@@ -1,11 +1,30 @@
-import pandas as pd
-import flask as Flask
-from rhino3dm import *
+from flask import Flask
+import ghhops_server as hs
 
-df = pd.DataFrame({'Name': ['Sailor'], 'Job': [
-    'Student']})
+# register hops app as middleware
+app = Flask(__name__)
+hops = hs.Hops(app)
 
 
-df1 = df.applymap(str.upper)
+@hops.component(
+    "/tree",
+    name="tree",
+    description="test",
+    icon="",
+    inputs=[
+        hs.HopsNumber("input", "i", "test")
+    ],
+    outputs=[
+        hs.HopsNumber("output", "o", "test")
+    ]
+)
+def tree(input):
+    return input
 
-print(df1)
+
+def main():
+    app.run()
+
+
+if __name__ == "__main__":
+    app.run()
